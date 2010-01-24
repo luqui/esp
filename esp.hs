121d8c4a8efcf89b50ae6676817dcaae90e9b886
Luke Palmer <lrpalmer@gmail.com>
2010-01-24 07:06:44 -0700

    Beta conversion.

--- esp.hs
+++ esp.hs
@@ -6,6 +6,7 @@ import Control.Applicative
 import ESP.UI
 import ESP.Command
 import qualified Data.Char as Char
+import qualified Data.Set as Set
 
 type Editor = Command UserInput CSyn
 
@@ -17,16 +18,45 @@ data AST
     deriving (Show)
 
 alphaConvert :: String -> String -> AST -> AST
+alphaConvert from to t | from == to = t
 alphaConvert from to (AApp t u) = AApp (alphaConvert from to t) (alphaConvert from to u)
 alphaConvert from to (ALam v t)
     | v == from = ALam v t
-    | v == to   = ALam v t  -- XXX capture
+    | v == to && from `Set.member` freeVars t  = 
+        let v' = primeId (freeVars t) v in 
+        alphaConvert from to (ALam v' (alphaConvert v v' t))
     | otherwise = ALam v (alphaConvert from to t)
 alphaConvert from to (AVar v)
-    | v == to = AVar v      -- XXX capture
+    | v == to = AVar ("*!" ++ v ++ "!*")  -- indicates that we have captured this variable
     | v == from = AVar to
     | otherwise = AVar v
 alphaConvert from to AHole = AHole
+
+freeVars :: AST -> Set.Set String
+freeVars (AApp t u) = freeVars t `Set.union` freeVars u
+freeVars (ALam v t) = Set.delete v (freeVars t)
+freeVars (AVar v) = Set.singleton v
+freeVars AHole = Set.empty
+
+primeId :: Set.Set String -> String -> String
+primeId frees x | x `Set.member` frees = primeId frees (x ++ "'")
+                | otherwise = x
+
+substitute :: String -> AST -> AST -> AST
+substitute var to (AApp t u) = AApp (substitute var to t) (substitute var to u)
+substitute var to (ALam v t) 
+    | v `Set.member` freeVars to = 
+        let v' = primeId (freeVars to `Set.union` freeVars t) v in
+        substitute var to (ALam v' (alphaConvert v v' t))
+    | otherwise = ALam v (substitute var to t)
+substitute var to (AVar v)
+    | var == v = to
+    | otherwise = AVar v
+substitute var to AHole = AHole
+
+betaExpand :: AST -> AST
+betaExpand (AApp (ALam v t) u) = substitute v u t
+betaExpand t = t
 
 data Complexity = Simple | Application | Lambda
     deriving (Eq,Ord)
@@ -85,6 +115,9 @@ mkEditor ast = More (highlight $ concrete ast) $ \ui@(UserInput key _) ->
         case key of
             W.KASCII 'h' -> mkEditor . (`AApp` u) =<< mapCommand (`cApp` concrete u) (mkEditor t)
             W.KASCII 'l' -> mkEditor . (t `AApp`) =<< mapCommand (concrete t `cApp`) (mkEditor u)
+            W.KASCII 'H' -> mkEditor t
+            W.KASCII 'L' -> mkEditor u
+            W.KASCII 'b' -> mkEditor (betaExpand (AApp t u))
             _ -> mkEditor (AApp t u)
     go (ALam v t) (UserInput key _) =
         case key of
