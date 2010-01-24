a7c87ec4cf82e19fa211a04d8e5e2e081e029701
Luke Palmer <lrpalmer@gmail.com>
2010-01-24 05:39:01 -0700

    Parsing actual expressions now.

--- esp.hs
+++ esp.hs
@@ -3,69 +3,71 @@
 import qualified Graphics.Vty.Widgets.All as W
 import qualified Graphics.Vty as W
 import Control.Applicative
-import Data.Monoid (Monoid(..))
 import ESP.UI
 import ESP.Command
-import qualified ESP.Parser as P
-import Data.Function (fix)
-import Debug.Trace
-import Control.Monad (MonadPlus(..))
+import qualified Data.Char as Char
 
+type Editor = Command UserInput W.AnyWidget
+
+data AST 
+    = App AST AST
+    | Lam String AST
+    | Var String
+    deriving (Show)
+
+showAST :: Bool -> Bool -> AST -> String
+showAST lp ap (App t u) = parensS ap $ showAST True False t ++ " " ++ showAST True True u
+showAST lp ap (Lam v t) = parensS lp $ "\\" ++ v ++ ". " ++ showAST False False t
+showAST lp ap (Var v) = v
+
+parensS True x = "(" ++ x ++ ")"
+parensS False x = x
 
 defaultAttr = W.Attr { W.style = W.SetTo 0, W.fore_color = W.SetTo W.white, W.back_color = W.SetTo W.black }
 highlightAttr = defaultAttr { W.style = W.SetTo 0, W.fore_color = W.SetTo W.black, W.back_color = W.SetTo W.white }
 
-inputParser :: P.Parser Char a -> Command UserInput String a
-inputParser = \p -> fix (go "" p)
+identifier :: [Char] -> String -> Editor String
+identifier seps = go
     where
-    go inp (P.Get c) undo = More inp $ \(UserInput key _) ->
-        let this = go inp (P.Get c) undo in
+    go inp = More (text defaultAttr inp <++> text highlightAttr " ") $ \(UserInput key _) ->
         case key of
-            W.KASCII ch -> go (inp ++ [ch]) (c ch) this
-            W.KBS -> undo
-            _ -> this
-    go inp (P.Result r P.Fail) undo = Done r
-    go inp (P.Result r p') undo = More inp $ \i@(UserInput key _) -> 
-            case key of
-                W.KEnter -> Done r
-                _ -> putback i (go inp p' undo )
-    go inp P.Fail undo = undo
+            W.KEnter -> Done inp
+            W.KASCII ch | ch `elem` seps -> Done inp
+                        | otherwise      -> go (inp ++ [ch])
+            W.KBS -> go (safeInit inp)
+            _ -> go inp
+    safeInit [] = []
+    safeInit xs = init xs
 
-parseDefnLhs :: P.Parser Char String
-parseDefnLhs = do
-    x <- P.identifier
-    P.whitespace
-    P.symbol "="
-    return x
+lambda :: Editor AST
+lambda = do
+    var <- mapCommand (\e -> text defaultAttr "\\" <++> e <++> text defaultAttr ".") $ identifier " ." ""
+    body <- mapCommand (text defaultAttr ("\\" ++ var ++ ". ") <++>) $ expr
+    return $ Lam var body
 
-inputDecl :: UI UserInput W.AnyWidget
-inputDecl =
-    runCommand (\s -> (text defaultAttr (s ++ " = ") <++>) <$> inputExpr) $ 
-        mapCommand itext (inputParser parseDefnLhs)
+parens :: Editor a -> Editor a
+parens = mapCommand (\e -> text defaultAttr "(" <++> e <++> text defaultAttr ")")
 
-data ExprHead
-    = Lambda String
-    | Parens
-    | Variable String
+simpleExpr :: Editor AST
+simpleExpr = More (text defaultAttr "") $ \(UserInput key _) ->
+        case key of
+            W.KASCII '(' -> parens expr
+            W.KASCII '\\' -> lambda
+            W.KASCII ch | Char.isAlpha ch -> Var <$> identifier " ()" [ch]
+            _ -> simpleExpr
 
-parseExprHead :: P.Parser Char ExprHead
-parseExprHead = (P.symbol "(" *> pure Parens) 
-        `mplus` (Lambda <$> lambda) 
-        `mplus` (Variable <$> P.identifier)
+expr :: Editor AST
+expr = go =<< simpleExpr
     where
-    lambda = P.symbol "\\" *> P.identifier <* P.symbol "."
+    go exp = More (text highlightAttr (showAST False False exp)) $ \inp@(UserInput key _) ->
+        case key of
+            W.KEnter -> Done exp
+            _ -> go =<< (exp `App`) <$> mapCommand (text defaultAttr (showAST True False exp) <++> text defaultAttr " " <++>) (putback inp simpleExpr)
+    
 
-inputExpr :: UI UserInput W.AnyWidget
-inputExpr =
-    runCommand cont $ mapCommand itext (inputParser parseExprHead)
-    where
-    cont (Lambda vname) = (text defaultAttr ("\\" ++ vname ++ ". ") <++>) <$> inputExpr
-    cont Parens = parenify <$> inputExpr
-    cont (Variable s) = (text defaultAttr (s ++ " ") <++>) <$> inputExpr
-    parenify x = text defaultAttr "(" <++> x <++> text defaultAttr ")"
-
-nothing :: UI UserInput W.AnyWidget
-nothing = UI (text defaultAttr "") (const nothing)
+commandTest :: (Show a) => Command UserInput W.AnyWidget a -> UI UserInput W.AnyWidget
+commandTest (Done a) = UI (text highlightAttr (show a)) (const (commandTest (Done a)))
+commandTest (More w c) = UI w (commandTest . c)
 
 (<++>) :: W.AnyWidget -> W.AnyWidget -> W.AnyWidget
 x <++> y = W.anyWidget (x W.<++> y)
@@ -78,4 +80,4 @@ itext s = text defaultAttr s <++> text highlightAttr " " <++> text defaultAttr "
 text :: W.Attr -> String -> W.AnyWidget
 text attr = W.anyWidget . W.text attr
 
-main = runVty inputDecl
+main = runVty (commandTest expr)
